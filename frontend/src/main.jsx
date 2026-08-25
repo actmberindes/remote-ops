f9840d2ec398b9a9d4685f761777601c4e65eb9a
actmberindes <act.mberindes@gmail.com>
2026-08-25 12:33:30 -0400

    Load asset cost preservation enhancement

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import './asset-ui-enhancer.js';
+import './asset-ui-cost-preserver.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
