c35edad75b417ea3ba044217e77154f10a44e850
actmberindes <act.mberindes@gmail.com>
2026-09-08 17:39:49 -0400

    Remove unstable screenshot DOM enhancer from React entrypoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,6 @@ import './live-view-smooth-enhancer.js';
 import './monitoring-view-enhancer.js';
 import './monitoring-rdp-label-enhancer.js';
 import './live-view-status-fullscreen-enhancer.js';
-import './screenshot-pagination-enhancer.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
