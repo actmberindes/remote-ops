748d4744d8d8f85abfed32180e2b9499f6955484
actmberindes <act.mberindes@gmail.com>
2026-09-08 17:33:47 -0400

    Restore Live View fullscreen and screenshot pagination

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,6 +10,8 @@ import './live-view-idle-enhancer.js';
 import './live-view-smooth-enhancer.js';
 import './monitoring-view-enhancer.js';
 import './monitoring-rdp-label-enhancer.js';
+import './live-view-status-fullscreen-enhancer.js';
+import './screenshot-pagination-enhancer.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
