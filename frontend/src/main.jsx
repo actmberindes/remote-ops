9eabe70d036307b8e9bb8bbc958fabbc8903eedd
actmberindes <act.mberindes@gmail.com>
2026-09-08 10:16:54 -0400

    Enable Live View full-screen and screenshot metadata

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,8 @@ import './live-view-idle-enhancer.js';
 import './live-view-smooth-enhancer.js';
 import './monitoring-view-enhancer.js';
 import './monitoring-rdp-label-enhancer.js';
+import './live-view-fullscreen-enhancer.js';
+import './screenshot-metadata-enhancer.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
