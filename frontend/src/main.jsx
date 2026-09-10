f5083395c8e7a22696f2627e8764dafccc6de886
actmberindes <act.mberindes@gmail.com>
2026-09-10 11:23:44 -0400

    Load screenshot current-user and slider enhancer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,6 +11,7 @@ import './live-view-smooth-enhancer.js';
 import './monitoring-view-enhancer.js';
 import './monitoring-rdp-label-enhancer.js';
 import './live-view-status-fullscreen-enhancer.js';
+import './screenshot-user-slider-enhancer.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
