e6ea47f718ed8f38268c89607a71206a24986979
actmberindes <act.mberindes@gmail.com>
2026-09-14 17:12:36 -0400

    Load device details UI terminology enhancer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,7 @@ import './monitoring-view-enhancer.js';
 import './monitoring-rdp-label-enhancer.js';
 import './live-view-status-fullscreen-enhancer.js';
 import './screenshot-user-slider-enhancer.js';
+import './device-details-ui-enhancer.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
